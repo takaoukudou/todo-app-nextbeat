0a170c8b2fd50c967114b768078050c2e02d3c12
Takaou Kudou <takao.kudo@nextbeat.net>
2022-03-11 18:53:33 +0900

    - 一覧表示のロジック修正(id => category_id) - 一覧表示画面で色を付与するロジックを修正 - 追加画面新規作成

--- app/controllers/ListController.scala
+++ app/controllers/ListController.scala
@@ -1,12 +1,18 @@
 package controllers
 
+import lib.model.ToDo
 import play.api.i18n.I18nSupport
 import play.api.mvc._
 import lib.persistence.ToDoRepository
 import lib.persistence.ToDoCategoryRepository
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText, shortNumber, text}
+
 import javax.inject._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import slick.profile.MyDBProfile
+
+case class ToDoFormData(title: String, body: String, category: Short)
 
 @Singleton
 class ListController @Inject() (
@@ -14,24 +20,24 @@ class ListController @Inject() (
 )(implicit ec: ExecutionContext)
     extends BaseController
     with I18nSupport {
+
+  val stateMap = Map((0, "TODO(着手前)"), (1, "進行中"), (2, "完了"))
+
   def list() = Action async { implicit request: Request[AnyContent] =>
     for {
       toDos <- ToDoRepository()(MyDBProfile).all()
       toDoCategories <- ToDoCategoryRepository()(MyDBProfile).all()
     } yield {
       val toDoInfoList = toDos.map(toDo => {
-        toDoCategories.find(toDoCategory => toDo.id == toDoCategory.id) match {
+        toDoCategories.find(toDoCategory =>
+          toDo.v.categoryId == toDoCategory.id
+        ) match {
           case Some(toDoCategory) =>
             (
               toDo.v.id,
               toDo.v.title,
               toDo.v.body,
-              toDo.v.state match {
-                case 0 => "TODO(着手前)"
-                case 1 => "進行中"
-                case 2 => "完了"
-                case _ => ""
-              },
+              stateMap(toDo.v.state),
               toDoCategory.v.name,
               toDoCategory.v.color
             )
@@ -39,5 +45,43 @@ class ListController @Inject() (
       })
       Ok(views.html.todo.List(toDoInfoList))
     }
+  }
+
+  val form = Form(
+    mapping(
+      "title" -> nonEmptyText(maxLength = 255),
+      "body" -> text,
+      "category" -> shortNumber
+    )(ToDoFormData.apply)(ToDoFormData.unapply)
+  )
+
+  /** 登録処理実を行う
+    */
+  def store() = Action async { implicit request: Request[AnyContent] =>
+    form
+      .bindFromRequest()
+      .fold(
+        // 処理が失敗した場合に呼び出される関数
+        (formWithErrors: Form[ToDoFormData]) => {
+          Future.successful(BadRequest(views.html.todo.Store(formWithErrors)))
+        },
+        // 処理が成功した場合に呼び出される関数
+        (toDoFormData: ToDoFormData) => {
+          for {
+            // データを登録。returnのidは不要なので捨てる
+            _ <- ToDoRepository()(MyDBProfile)
+              .add(
+                ToDo(
+                  toDoFormData.category,
+                  toDoFormData.title,
+                  Option(toDoFormData.body),
+                  0
+                )
+              )
+          } yield {
+            Redirect(routes.ListController.list())
+          }
+        }
+      )
   }
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,11 @@
- function setColorClass(targetId) {
+window.addEventListener('load',function(){
     const colorMap = {
         1:"category-front",
         2:"category-back",
         3:"category-infra"
     }
-    const category = document.getElementById(targetId)
-    category.classList.add(colorMap[category.getElementsByClassName("category_color")[0].value])
-}
-
-setColorClass("category1")
-setColorClass("category2")
-setColorClass("category3")
+    const categories = document.getElementsByClassName("category")
+    for(const category of categories){
+        category. classList.add(colorMap[category.getElementsByClassName("category_color")[0].value])
+    }
+})
